add inference, add metrics calculation from GaoXiang, fix ppl computation bugs

main.py
@@ -3,7 +3,7 @@ from model.Decoder import RNNBaseDecoder, Generator
 from model.EncoderDecoder import RNNBaseSeq2Seq
 from model.Embedding import Embedding
 from data.dataset import seq2seq_dataset, load_iwslt
-from train import train, evaluate
+from train import train, evaluate, inference
 import torch.optim as optim
 import time
 import math
@@ -17,11 +17,17 @@ from datetime import datetime
 from dateutil import tz, zoneinfo
 import json
 from torch.utils.tensorboard import SummaryWriter
+from transformers.models.bert.tokenization_bert import BertTokenizer
+import logging
+import coloredlogs
+from nlp_metrics import calc_metrics
+logger = logging.getLogger(__name__)
+coloredlogs.install(level='INFO', logger=logger)
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='allennlp seq2seq'
+        description='seq2seq'
     )
 
     parser.add_argument('--gpu', default=-1, type=int, help='which GPU to use, -1 means using CPU')
@@ -29,12 +35,15 @@ def main():
     parser.add_argument('--bs', default=64, type=int, help='batch size')
     parser.add_argument('--emb_dim', default=300, type=int, help='embedding dim')
     parser.add_argument('--hid_dim', default=300, type=int, help='hidden dim of lstm')
+    parser.add_argument('--birnn', action='store_true', help='whether to use bidirectional rnn, default False')
+    parser.add_argument('--n_layers', default=1, type=int, help='layer num of encoder and decoder')
     parser.add_argument('--dropout', default=0.5, type=float, help='dropout ratio')
     parser.add_argument('--n_epochs', default=30, type=int, help='num of train epoch')
     parser.add_argument('--min_freq', default=1, type=int, help='minimal occur times for vocabulary')
     parser.add_argument('--clip', default=None, type=float, help='grad clip')
     parser.add_argument('--maxlen', default=None, type=int, help='max length of text')
     parser.add_argument('--dataset_dir_path', default=None, type=str, help='path to directory where data file is saved')
+    parser.add_argument('--tokenizer', default='spacy_en', type=str, help='which tokenizer to use for the dataset')
     parser.add_argument('--train_file', default=None, type=str, help='train file name')
     parser.add_argument('--valid_file', default=None, type=str, help='valid file name')
     parser.add_argument('--test_file', default=None, type=str, help='test file name')
@@ -48,15 +57,22 @@ def main():
     parser.add_argument('--pretrained_embed_file', default=None, type=str, help='torchtext vector name')
     parser.add_argument('--warmup', default=0, type=int, help='warmup steps, 0 means not using NoamOpt')
     parser.add_argument('--cell_type', default='LSTM', type=str, help='cell type of encoder/decoder, LSTM or GRU')
-    parser.add_argument('--comment', default='', type=str, help='comment, will be used as suffix of save directory')
+    parser.add_argument('--comment', default='', type=str, help='comment, will be used as prefix of save directory')
     parser.add_argument('--smoothing', default=0.0, type=float, help='smoothing rate of computing kl div loss')
     parser.add_argument('--max_vocab_size', default=None, type=int, help='max size of vocab')
+    parser.add_argument('--serialize', action='store_true', help='whether to serialize examples and vocab')
+    parser.add_argument('--use_serialized', action='store_true', help='whether to use serialized dataset')
+    parser.add_argument('--model_path', default=None, type=str, help='restore model to continue training')
+    parser.add_argument('--global_step', default=0, type=int, help='global step for continuing training')
+    parser.add_argument('--inference', action='store_true', help='inference mode')
 
     args, unparsed = parser.parse_known_args()
     writer = None
     if args.save:
         tz_sh = tz.gettz('Asia/Shanghai')
-        save_dir = os.path.join(args.save_dir, 'run' + str(datetime.now(tz=tz_sh)).replace(":", "-").split(".")[0].replace(" ", '.') + args.comment)
+        save_dir = os.path.join(args.save_dir, args.comment + 'run' + str(datetime.now(tz=tz_sh)).replace(":", "-").split(".")[0].replace(" ", '.'))
+        if args.model_path:
+            save_dir = os.path.split(args.model_path)[0]
         args.save_dir = save_dir
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
@@ -67,15 +83,23 @@ def main():
     device = torch.device(args.gpu if (torch.cuda.is_available() and args.gpu >= 0) else 'cpu')
     args.device = device
 
-    #  from data.dataset import jieba_tokenize
-    #  dataset = seq2seq_dataset(args, tokenizer=jieba_tokenize)
-    dataset = seq2seq_dataset(args)
+    if args.tokenizer == 'spacy_en':
+        dataset = seq2seq_dataset(args)
+    elif args.tokenizer == 'jieba':
+        from data.dataset import jieba_tokenize
+        dataset = seq2seq_dataset(args, tokenizer=jieba_tokenize)
+    elif args.tokenizer == 'bert':
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        dataset = seq2seq_dataset(args, tokenizer=tokenizer.tokenize)
+
     #  dataset = load_iwslt(args)
+
     SRC = dataset['fields']['src']
     TGT = dataset['fields']['tgt']
+
     EMB_DIM = args.emb_dim
     HID_DIM = args.hid_dim
-    N_LAYERS = 1
+    N_LAYERS = args.n_layers
     ENC_DROPOUT = args.dropout
     DEC_DROPOUT = args.dropout
     SRC_PAD_IDX = SRC.vocab.stoi[SRC.pad_token]
@@ -93,41 +117,54 @@ def main():
         src_embedding.lut.weight.data.copy_(src_pretrained_vectors)
         tgt_embedding.lut.weight.data.copy_(tgt_pretrained_vectors)
         print("pretrained vectors loaded successfully!")
-    enc = RNNBaseEncoder(args.cell_type, EMB_DIM, HID_DIM, N_LAYERS, bidirectional=False, dropout=ENC_DROPOUT)
+
+    enc = RNNBaseEncoder(args.cell_type, EMB_DIM, HID_DIM, N_LAYERS, bidirectional=args.birnn, dropout=ENC_DROPOUT)
     dec = RNNBaseDecoder(args.cell_type, EMB_DIM, HID_DIM, N_LAYERS, dropout=DEC_DROPOUT)
     generator = Generator(HID_DIM, len(TGT.vocab))
     model = RNNBaseSeq2Seq(enc, dec, src_embedding, tgt_embedding, generator).to(device)
+    if args.model_path is not None:
+        logger.info(f"Restore model from {args.model_path}...")
+        model.load_state_dict(torch.load(args.model_path, map_location={'cuda:0': 'cuda:' + str(args.gpu)}))
+
+    weight = torch.ones(len(TGT.vocab), device=args.device)
+    weight[TGT_PAD_IDX] = 0
+    criterion = nn.NLLLoss(reduction='sum', ignore_index=TGT_PAD_IDX, weight=weight)
+    #  criterion = LabelSmoothing(args, len(TGT.vocab), padding_idx=TGT_PAD_IDX, smoothing=args.smoothing)
+
+    if args.inference:
+        try:
+            assert args.model_path is not None
+        except AssertionError:
+            logger.error("If you want to do inference, you must offer a trained model's path!")
+        finally:
+            inference(args, model, dataset['valid_iterator'], criterion, fields=dataset['fields'], mode='valid')
+            inference(args, model, dataset['test_iterator'], criterion, fields=dataset['fields'], mode='test')
+            return 0
 
     print(f'The model has {count_parameters(model):,} trainable parameters')
     optimizer = AdamOptimizer(model.parameters(), lr=args.lr, weight_decay=args.l2, max_grad_norm=args.clip)
     #  optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.l2)
     if args.warmup > 0:
         optimizer = NoamOptimWrapper(args.hid_dim, 1, args.warmup, optimizer)
-    #  criterion = nn.CrossEntropyLoss(ignore_index=TGT_PAD_IDX, reduction='sum')
-    criterion = LabelSmoothing(len(TGT.vocab), padding_idx=TGT_PAD_IDX, smoothing=args.smoothing)
+    if args.global_step > 0:
+        logger.info(f'Global step start from {args.global_step}')
+        optimizer._step = args.global_step
 
     # TODO 取消hard-code式保存最佳指标
-    best_epoch = 0
+    best_global_step = 0
     best_valid_loss = float('inf')
-    best_bleu_1 = 0
-    best_bleu_2 = 0
-    best_dist_1 = 0
-    best_dist_2 = 0
-    global_step = 0
-    #  bleu_1 = BLEU(ngram_weights=[1, 0, 0, 0], exclude_indices={TGT_PAD_IDX, TGT.vocab.stoi[TGT.eos_token], TGT.vocab.stoi[TGT.init_token]})
-    #  bleu_2 = BLEU(ngram_weights=[0.5, 0.5, 0, 0], exclude_indices={TGT_PAD_IDX, TGT.vocab.stoi[TGT.eos_token], TGT.vocab.stoi[TGT.init_token]})
-    #  bleu = {"bleu_1": bleu_1, "bleu_2": bleu_2}
-    bleu = BLEU(n=2, exclude_indices={TGT_PAD_IDX, TGT.vocab.stoi[TGT.eos_token], TGT.vocab.stoi[TGT.init_token]})
-    dist = Distinct(exclude_tokens={TGT_PAD_IDX, TGT.vocab.stoi[TGT.eos_token], TGT.vocab.stoi[TGT.init_token]})
+    best_test_loss = float('inf')
+    global_step = optimizer._step
 
     for epoch in range(N_EPOCHS):
         start_time = time.time()
 
         train_metrics = train(args, model, dataset['train_iterator'], optimizer, criterion, fields=dataset['fields'], writer=writer)
-        valid_metrics = evaluate(args, model, dataset['valid_iterator'], criterion, bleu=bleu, fields=dataset['fields'], dist=dist)
-        test_metrics = evaluate(args, model, dataset['test_iterator'], criterion, bleu=bleu, fields=dataset['fields'], dist=dist)
-        end_time = time.time()
         global_step += len(dataset['train_iterator'])
+        args.global_step = global_step
+        valid_metrics = evaluate(args, model, dataset['valid_iterator'], criterion, fields=dataset['fields'])
+        test_metrics = evaluate(args, model, dataset['test_iterator'], criterion, fields=dataset['fields'])
+        end_time = time.time()
         epoch_mins, epoch_secs = epoch_time(start_time, end_time)
         print(f'Epoch: {epoch + 1:02} | Global step: {global_step} | Time: {epoch_mins}m {epoch_secs}s')
         for metrics, mode in zip([train_metrics, valid_metrics, test_metrics], ['Train', 'Valid', 'Test']):
@@ -138,25 +175,41 @@ def main():
             write_metrics_to_writer(valid_metrics, writer, global_step, mode='Valid')
             write_metrics_to_writer(test_metrics, writer, global_step, mode='Test')
             best_valid_loss = valid_metrics['epoch_loss']  if valid_metrics['epoch_loss'] < best_valid_loss else best_valid_loss
-            best_epoch = epoch if valid_metrics['epoch_loss'] == best_valid_loss else best_epoch
-            best_bleu_1 = valid_metrics['bleu_1'] if valid_metrics['bleu_1'] > best_bleu_1 else best_bleu_1
-            best_bleu_2 = valid_metrics['bleu_2'] if valid_metrics['bleu_2'] > best_bleu_2 else best_bleu_2
-            best_dist_1 = valid_metrics['dist_1'] if valid_metrics['dist_1'] > best_dist_1 else best_dist_1
-            best_dist_2 = valid_metrics['dist_2'] if valid_metrics['dist_2'] > best_dist_2 else best_dist_2
+            best_test_loss = test_metrics['epoch_loss'] if test_metrics['epoch_loss'] < best_test_loss else best_test_loss
+            best_global_step = global_step if valid_metrics['epoch_loss'] == best_valid_loss else best_global_step
+
             torch.save(model.state_dict(), os.path.join(save_dir, f'model_global_step-{global_step}.pt'))
-            with open(os.path.join(save_dir, f'log_epoch-{epoch+1}_global_step-{global_step}.txt'), 'w') as log_file:
-                valid_metrics['Best Epoch'] = best_epoch + 1
-                valid_metrics['Best Valid Loss'] = best_valid_loss
-                valid_metrics['Best PPL'] = math.exp(best_valid_loss)
-                valid_metrics['Best BLEU-1'] = best_bleu_1
-                valid_metrics['Best BLEU-2'] = best_bleu_2
-                valid_metrics['Best Dist-1'] = best_dist_1
-                valid_metrics['Best Dist-2'] = best_dist_2
+            with open(os.path.join(save_dir, f'log_global_step-{global_step}.txt'), 'w') as log_file:
+                valid_metrics['Best Global Step'] = best_global_step
+                valid_metrics['Best Loss'] = best_valid_loss
+
+                test_metrics['Best Loss'] = best_test_loss
+                test_metrics['Best PPL'] = math.exp(best_test_loss)
+
+                inference(args, model, dataset['valid_iterator'], criterion, fields=dataset['fields'], mode='valid')
+                inference(args, model, dataset['test_iterator'], criterion, fields=dataset['fields'], mode='test')
+
+                valid_path_hyp = os.path.join(args.save_dir, 'responses-valid.txt')
+                test_path_hyp = os.path.join(args.save_dir, 'responses-test.txt')
+                valid_path_ref = os.path.join(args.save_dir, 'answers-valid.txt')
+                test_path_ref = os.path.join(args.save_dir, 'answers-test.txt')
+
+                other_valid_metrics = calc_metrics(path_refs=valid_path_ref, path_hyp=valid_path_hyp)
+                other_test_metrics = calc_metrics(path_refs=test_path_ref, path_hyp=test_path_hyp)
+                valid_metrics.update(other_valid_metrics)
+                test_metrics.update(other_test_metrics)
+
+                os.remove(os.path.join(args.save_dir, 'posts-valid.txt'))
+                os.remove(os.path.join(args.save_dir, 'posts-test.txt'))
+                os.remove(valid_path_hyp)
+                os.remove(valid_path_ref)
+                os.remove(test_path_hyp)
+                os.remove(test_path_ref)
 
                 for metric, performance in valid_metrics.items():
-                    log_file.write(f'{metric}: {performance}\n')
-                #  for metrics, mode in zip([train_metrics, valid_metrics, test_metrics], ['Train', 'Valid', 'Test']):
-                #      write_metrics(metrics, log_file, mode=mode)
+                    log_file.write(f'Valid {metric}: {performance}\n')
+                for metric, performance in test_metrics.items():
+                    log_file.write(f'Test {metric}: {performance}\n')
 
 
 if __name__ == '__main__':

metrics.py
@@ -140,7 +140,7 @@ def calc_diversity(path_hyp):
 				tokens[n] += 1
 	div1 = 1. * len(types[0].keys())/tokens[0]
 	div2 = 1. * len(types[1].keys())/tokens[1]
-	return [div1, div2]
+	return [(div1, len(types[0].keys())), (div2, len(types[1].keys()))]
 
 
 def calc_nltk_bleu(path_refs, path_hyp, n_lines):
@@ -173,19 +173,20 @@ def calc_nltk_bleu(path_refs, path_hyp, n_lines):
 	return bleu
 
 
-def nlp_metrics(path_refs, path_hyp, fld_out='temp',  n_lines=None):
-	print('calc_nist_bleu')
-	nist, sbleu = calc_nist_bleu(path_refs, path_hyp, fld_out, n_lines)
+def calc_nlp_metrics(path_refs, path_hyp, fld_out='temp',  n_lines=None):
+	#  print('calc_nist_bleu')
+	#  nist, sbleu = calc_nist_bleu(path_refs, path_hyp, fld_out, n_lines)
 	print('calc_nltk_bleu')
 	bleu = calc_nltk_bleu(path_refs, path_hyp, n_lines)
-	print('calc_meteor')
-	meteor = calc_meteor(path_refs, path_hyp, fld_out, n_lines)
+	#  print('calc_meteor')
+	#  meteor = calc_meteor(path_refs, path_hyp, fld_out, n_lines)
 	#meteor = np.nan
 	print('calc others')
 	entropy = calc_entropy(path_hyp, n_lines)
 	div = calc_diversity(path_hyp)
 	avg_len = calc_len(path_hyp, n_lines)
-	return nist, sbleu, bleu, meteor, entropy, div, avg_len
+	#  return nist, sbleu, bleu, meteor, entropy, div, avg_len
+	return bleu, entropy, div, avg_len
 
 
 def _write_merged_refs(paths_in, path_out, n_lines=None):

train.py
@@ -3,6 +3,7 @@ import torch
 import math
 import time
 import torch.nn.functional as F
+from utils import write_src_tgt_to_file, write_pred_to_file
 
 
 def train(args, model, iterator, optimizer, criterion, fields, writer=None):
@@ -12,6 +13,8 @@ def train(args, model, iterator, optimizer, criterion, fields, writer=None):
     total_loss = 0
     total_tokens = 0
     tgt_padding_idx = fields['tgt'].vocab.stoi[fields['tgt'].pad_token]
+    vocab_size = len(fields['tgt'].vocab)
+
     for i, batch in enumerate(iterator):
         src, src_len = batch.src
         # tgt = [tgt len, batch size]
@@ -22,19 +25,23 @@ def train(args, model, iterator, optimizer, criterion, fields, writer=None):
 
         # output = [tgt len, batch size, vocab_size]
         output = model(src, src_len.cpu().long(), tgt, teacher_forcing_ratio=args.teaching_rate)
-        # 用于labelsmoothing criterion
+        # 用于求KL-DIV Loss或NLL Loss需要先求log softmax
         output = F.log_softmax(output, dim=-1)
 
         #  output = model.forward_parallel(src, src_len.cpu().long(), tgt)
-        vocab_size = len(fields['tgt'].vocab)
         # output = [(tgt len - 1) * batch size, vocab_size]
         # tgt = [(tgt len - 1) * batch size]
         output = output[1:].reshape(-1, vocab_size)
         tgt = tgt[1:].reshape(-1)
 
-        loss, ce_loss = criterion(output, tgt)
-        ce_loss /= ntokens
-        loss /= ntokens
+        if args.smoothing > 0:
+            loss, ce_loss = criterion(output, tgt)
+            loss /= ntokens
+            ce_loss /= ntokens
+        else:
+            loss = criterion(output, tgt)
+            loss /= ntokens
+            ce_loss = loss
         loss.backward()
 
         optimizer.step()
@@ -49,17 +56,20 @@ def train(args, model, iterator, optimizer, criterion, fields, writer=None):
 
         total_loss += ce_loss.item() * ntokens
         #  break
-    return {'epoch_loss': total_loss / total_tokens,
+
+    return {
+            'epoch_loss': total_loss / total_tokens,
             'PPL': math.exp(total_loss / total_tokens),
             }
 
 
-def evaluate(args, model, iterator, criterion, fields, bleu=None, dist=None):
+def evaluate(args, model, iterator, criterion, fields):
     model.eval()
 
     total_loss = 0
     total_tokens = 0
     tgt_padding_idx = fields['tgt'].vocab.stoi[fields['tgt'].pad_token]
+    vocab_size = len(fields['tgt'].vocab)
 
     with torch.no_grad():
         for batch in tqdm(iterator):
@@ -68,35 +78,56 @@ def evaluate(args, model, iterator, criterion, fields, bleu=None, dist=None):
             tgt, tgt_len = batch.tgt
             ntokens = (tgt[1:] != tgt_padding_idx).data.sum()
             total_tokens += ntokens
-            vocab_size = len(fields['tgt'].vocab)
             # output = [tgt len, batch size, vocab_size]
             output = model(src, src_len.cpu().long(), tgt, 0)  # turn off teacher forcing
+
             # pred = [batch size, tgt len - 1]
-            pred = output[1:].argmax(-1).T
-            if bleu:
-                gold = tgt[1:].T
-                bleu.forward(predictions=pred, gold_targets=gold)
-            if dist:
-                dist.forward([hyps for hyps in pred.detach().cpu().numpy()])
+            #  pred = output[1:].argmax(-1).T
 
             # output = [(tgt len - 1) * batch size, vocab_size]
             # tgt = [(tgt len - 1) * batch size]
             output = output[1:].reshape(-1, vocab_size)
             tgt = tgt[1:].reshape(-1)
 
-            # 用于labelsmoothing criterion
+            # 用于求KL-DIV Loss或NLL Loss需要先求log softmax
             output = F.log_softmax(output, dim=-1)
-            loss, ce_loss = criterion(output, tgt)
+            if args.smoothing > 0:
+                _, ce_loss = criterion(output, tgt)
+            else:
+                ce_loss = criterion(output, tgt)
             total_loss += ce_loss.item()
             #  break
-    metrics = {'epoch_loss': total_loss / total_tokens,
-               'PPL': math.exp(total_loss / total_tokens),
-               }
-    if bleu:
-        metrics.update(bleu.get_metric(reset=True))
-        #  metrics['bleu'] = bleu.get_metric(reset=True)['BLEU']
-    if dist:
-        dist_n = dist.get_metric(reset=True)
-        metrics.update(dist_n)
+
+    metrics = {
+            'epoch_loss': total_loss / total_tokens,
+            'PPL': math.exp(total_loss / total_tokens),
+            }
 
     return metrics
+
+
+def inference(args, model, iterator, criterion, fields, mode):
+    model.eval()
+    src_vocab = fields['src'].vocab
+    tgt_vocab = fields['tgt'].vocab
+
+    with torch.no_grad():
+        for batch in tqdm(iterator):
+            src_num_lines = write_src_tgt_to_file(args, batch, src_vocab, tgt_vocab, mode)
+            src, src_len = batch.src
+            # tgt = [tgt len, batch size]
+            tgt, tgt_len = batch.tgt
+            # output = [tgt len, batch size, vocab_size]
+            output = model(src, src_len.cpu().long(), tgt, 0)  # turn off teacher forcing
+
+            # pred = [batch size, tgt len - 1]
+            pred = output[1:].argmax(-1).T
+            num_lines = write_pred_to_file(args, pred, fields['tgt'].vocab, mode)
+            try:
+                assert src_num_lines == num_lines
+            except AssertionError:
+                print('src num line', src_num_lines)
+                print('pred num line', num_lines)
+                print('src', src.size(), src)
+                print('pred', pred.size(), pred)
+

utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch.nn as nn
 import copy
 from collections import Counter
+import os
 
 
 def corrcoef(x, rowvar=True):
@@ -190,12 +191,19 @@ class BLEU:
 
 class LabelSmoothing(nn.Module):
     "Implement label smoothing using KL div loss"
-    def __init__(self, size, padding_idx, smoothing=0.0):
+    def __init__(self, args, size, padding_idx, smoothing=0.0):
         super(LabelSmoothing, self).__init__()
-        assert smoothing >= 0 and smoothing < 1
+        assert smoothing > 0 and smoothing < 1
         self.criterion = nn.KLDivLoss(reduction='sum')
-        self.ce_criterion = nn.NLLLoss(reduction='sum', ignore_index=padding_idx)
-        # self.ce_criterion = nn.CrossEntropyLoss(reduction='sum', ignore_index=padding_idx)
+        self.criterion.to(args.device)
+        # ignore_index只是让padding_idx的梯度不参与计算，并没有把padding_idx上的loss排除在总的loss之外，
+        # 所以这样计算得到的loss是偏大的，应该再设置weight为[0,1,1,…]（0表示padding_idx），这样就能保证总的loss不包括padding_idx上的loss
+        weight = torch.ones(size)
+        weight[padding_idx] = 0
+        weight.to(args.device)
+        self.ce_criterion = nn.NLLLoss(reduction='sum', ignore_index=padding_idx, weight=weight)
+        self.ce_criterion.to(args.device)
+        
         self.padding_idx = padding_idx
         self.confidence = 1.0 - smoothing
         self.smoothing = smoothing
@@ -222,4 +230,47 @@ class LabelSmoothing(nn.Module):
         #  避免模型变得太过confident，模型学着预测时变得更不确定，这对ppl有伤害，但是能够提升预测的准确性和BLEU分数
         #  当x的概率分布很尖锐时，loss将变大
         return self.criterion(x, torch.autograd.Variable(true_dist, requires_grad=False)), self.ce_criterion(x, target.long())
+
+
+def write_src_tgt_to_file(args, batch, src_vocab, tgt_vocab, mode='valid'):
+    num_lines = 0
+    with open(os.path.join(args.save_dir, 'posts-' + mode + '.txt'), 'a') as fp:
+        with open(os.path.join(args.save_dir, 'answers-'+ mode + '.txt'), 'a') as fa:
+            src, src_len = batch.src
+            src = src.T
+            # tgt = [tgt len, batch size]
+            tgt, tgt_len = batch.tgt
+            tgt = tgt.T
+            src = [[src_vocab.itos[x] for x in ex] for ex in src]
+            tgt = [[tgt_vocab.itos[x] for x in ex] for ex in tgt]
+            for k in range(len(src)):
+                fp.write(' '.join(src[k][:src_len[k]]).strip() + '\n')
+                fa.write(' '.join(tgt[k][1:tgt_len[k] - 1]).strip() + '\n')
+                num_lines += 1
+    return num_lines
+
+
+def write_pred_to_file(args, response, tgt_vocab, mode='valid'):
+    num_lines = 0
+    with open(os.path.join(args.save_dir, 'responses-' + mode + '.txt'), 'a') as fr:
+        #  response = [batch_size, len]
+        res = []
+        TGT_EOS_IDX = tgt_vocab.stoi['<eos>']
+
+        for ex in response:
+            wrote = False
+            cur = []
+            for x in ex:
+                if x != TGT_EOS_IDX:
+                    cur.append(tgt_vocab.itos[x])
+                else:
+                    fr.write(' '.join(cur).strip() + '\n')
+                    wrote = True
+                    num_lines += 1
+                    break
+            if not wrote:
+                fr.write(' '.join(cur).strip() + '\n')
+                num_lines += 1
+
+    return num_lines
 